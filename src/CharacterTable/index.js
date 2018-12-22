a34a7bd38b97668e4843d937e5dee4d74e3539e0
Tobiah Rex <tobiah.rex@gmail.com>
2018-12-21 17:19:33 -0800

    updated CharacterTable.

--- src/CharacterTable/index.js
+++ src/CharacterTable/index.js
@@ -1,29 +1,81 @@
 import React from 'react';
-import
+import personTestAvatar1 from '../assets/images/guess-who-alex.JPG';
+import personTestAvatar2 from '../assets/images/guess-who-joe.JPG';
+import personTestAvatar3 from '../assets/images/guess-who-maria.JPG';
+
+
 
 class CharacterTable extends React.Component {
-  render() {
-    return (
-      <table style="width:100%">
-        {/* <tr>
-          <th>Firstname</th>
-        </tr> */}
-        <tr>
-          <td><img src={personTestAvatar1} /></td>
-          <td><img src={personTestAvatar2} /></td>
-          <td><img src={personTestAvatar3} /></td>
-          <td><img src={personTestAvatar1} /></td>
-          <td><img src={personTestAvatar2} /></td>
-          <td><img src={personTestAvatar3} /></td>
-          <td><img src={personTestAvatar1} /></td>
-          <td><img src={personTestAvatar2} /></td>
-        </tr>
-        <tr>
-          <td>Eve</td>
-          <td>Jackson</td>
-          <td>94</td>
-        </tr>
-      </table>
-    )
+  renderImages = (avatarCollec) => {
+    for (let i = 8; i > 0; i--) {
+      return (
+        <img src={avatarCollec[i]} alt={`Character ${i - 1}`} class="board-avatar" />
+      )
+    }
   }
+
+  render() {
+    let avatars = [
+      personTestAvatar1,
+      personTestAvatar2,
+      personTestAvatar3
+    ];
+
+
+    return (
+      <table style={
+        {
+          width: '100%'
+        }
+      }>
+      {/* <tr>
+        <th>Firstname</th>
+      </tr> */}
+      <tr>
+        <td class="avatar-boundry" align="middle">
+          <div class="board-avatar-container">
+            <img src={personTestAvatar1} alt="Character 1" class="board-avatar" />
+            <img src={personTestAvatar2} alt="Character 2" class="board-avatar" />
+            <img src={personTestAvatar3} alt="Character 3" class="board-avatar" />
+            <img src={personTestAvatar1} alt="Character 1" class="board-avatar" />
+            <img src={personTestAvatar2} alt="Character 2" class="board-avatar" />
+            <img src={personTestAvatar3} alt="Character 3" class="board-avatar" />
+            <img src={personTestAvatar1} alt="Character 1" class="board-avatar" />
+            <img src={personTestAvatar2} alt="Character 2" class="board-avatar" />
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td class="avatar-boundry" align="middle">
+          <div class="board-avatar-container">
+            <img src={personTestAvatar1} alt="Character 1" class="board-avatar" />
+            <img src={personTestAvatar2} alt="Character 2" class="board-avatar" />
+            <img src={personTestAvatar3} alt="Character 3" class="board-avatar" />
+            <img src={personTestAvatar1} alt="Character 1" class="board-avatar" />
+            <img src={personTestAvatar2} alt="Character 2" class="board-avatar" />
+            <img src={personTestAvatar3} alt="Character 3" class="board-avatar" />
+            <img src={personTestAvatar1} alt="Character 1" class="board-avatar" />
+            <img src={personTestAvatar2} alt="Character 2" class="board-avatar" />
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td class="avatar-boundry" align="middle">
+          <div class="board-avatar-container">
+            <img src={personTestAvatar1} alt="Character 1" class="board-avatar" />
+            <img src={personTestAvatar2} alt="Character 2" class="board-avatar" />
+            <img src={personTestAvatar3} alt="Character 3" class="board-avatar" />
+            <img src={personTestAvatar1} alt="Character 1" class="board-avatar" />
+            <img src={personTestAvatar2} alt="Character 2" class="board-avatar" />
+            <img src={personTestAvatar3} alt="Character 3" class="board-avatar" />
+            <img src={personTestAvatar1} alt="Character 1" class="board-avatar" />
+            <img src={personTestAvatar2} alt="Character 2" class="board-avatar" />
+          </div>
+        </td>
+      </tr>
+    </table>
+  )
 }
+}
+
+export default CharacterTable;
